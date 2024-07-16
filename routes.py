00bcfb805b8abd626f8e0db4f71dc2332cc1cdae
Lê Đình Bảo <120027008+ledinhbaoute@users.noreply.github.com>
2024-07-16 21:05:46 +0700

    Update routes.py

--- routes.py
+++ routes.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 from app import app
 from recommend_comic import recommend_comic
-from nsfw_classifer import predict_image
+# from nsfw_classifer import predict_image
 from azure_classifer import azure_predict_image
 
 @app.route('/user/<string:user_id>', methods=['GET'])
@@ -13,24 +13,24 @@ def fetch_user(user_id):
         return jsonify({'message': 'User not found or no history available'}), 404
     return jsonify(recommendations)
 
-@app.route('/checking_image', methods=['POST'])
-def predict():
-    image_file = request.files.get('image')
+# @app.route('/checking_image', methods=['POST'])
+# def predict():
+#     image_file = request.files.get('image')
         
-    if not image_file:
-        return jsonify({'error': 'No image uploaded'}), 400
+#     if not image_file:
+#         return jsonify({'error': 'No image uploaded'}), 400
         
-    try:
-        # Đọc ảnh từ file
-        file_bytes = np.fromstring(image_file.read(), np.uint8)
-        input_image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-        prediction = predict_image(input_image)
-        if prediction=='SFW':
-            return jsonify({'SFW': True, 'filename': image_file.filename}), 200
-        else:
-            return jsonify({'SFW': False, 'filename': image_file.filename}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+#     try:
+#         # Đọc ảnh từ file
+#         file_bytes = np.fromstring(image_file.read(), np.uint8)
+#         input_image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+#         prediction = predict_image(input_image)
+#         if prediction=='SFW':
+#             return jsonify({'SFW': True, 'filename': image_file.filename}), 200
+#         else:
+#             return jsonify({'SFW': False, 'filename': image_file.filename}), 200
+#     except Exception as e:
+#         return jsonify({'error': str(e)}), 500
 
 @app.route('/azure_checking_image', methods=['POST'])
 def azure_predict():

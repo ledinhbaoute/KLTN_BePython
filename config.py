eb5dbc93d44727f544719a118df880df0b9fa137
Lê Đình Bảo <120027008+ledinhbaoute@users.noreply.github.com>
2024-07-16 20:53:36 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://root:szHQAXGnGXZJEOBaJQmFKxuEbSxEgFYm@monorail.proxy.rlwy.net:27074/railway')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://root:szHQAXGnGXZJEOBaJQmFKxuEbSxEgFYm@monorail.proxy.rlwy.net:27074/railway')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
